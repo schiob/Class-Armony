b867ff4574b009716ddf8edf2e4c6aab80574d8a
red-mat <jesus.ing.rodriguez@gmail.com>
2023-04-03 21:21:31 -0300

    refactor: add global css folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
+import "css/index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
